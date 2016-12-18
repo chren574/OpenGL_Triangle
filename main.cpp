24f382f615170c3faf569b66cf4861a068ffa4ae
Chistoffer Engelbrektsson <chren574@student.liu.se>
2016-12-18 22:10:35 +0100

    A triangle!

--- main.cpp
+++ main.cpp
@@ -10,6 +10,24 @@
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
+// Shaders
+const GLchar* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 position;\n"
+	"void main()\n"
+	"{\n"
+	"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+	"}\0";
+
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+	"out vec4 color;\n"
+	"void main()\n"
+	"{\n"
+	"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n\0";
+
+// Window dimensions
+const int WIDTH = 800, HEIGHT = 600;
+
 int main()
 {
 	std::cout << "Init\n";
@@ -23,7 +41,7 @@ int main()
 	// Not resizeble by the user
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 	
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
 	if (window == nullptr)
 	{
 		std::cout << "Failed to create GLFW window\n";
@@ -50,6 +68,111 @@ int main()
 
 	glViewport(0, 0, width, height);
 
+	std::cout << "Create vertex shader\n";
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	// Attach the shader source code to the shader object
+	// Arg 0: The glShaderSource function takes the shader object to compile to as its first argument.
+	// Arg 1: The second argument specifies how many strings we're passing as source code, which is only one. 
+	// Arg 2: The third parameter is the actual source code of the vertex shader
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// Error check
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	std::cout << "Create vertex shader\n";
+	GLuint fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	std::cout << "Link shaders\n";
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	// Link the shaders
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::LINK::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Activate program
+	glUseProgram(shaderProgram);
+
+	// Clean
+	// The data hace been sent
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	// Set up vertex data (and buffer(s)) and attribute pointers
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, 0.0f, // Left  
+		0.5f, -0.5f, 0.0f, // Right 
+		0.0f,  0.5f, 0.0f  // Top   
+	};
+
+	std::cout << "Create Buffers\n";
+	// Vertex Buffer Object, Vertex Array Object, Element Buffer Objects
+	GLuint VBO, VAO;
+	// Creates a ID for the buffer 
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	//glGenBuffers(1, &EBO);
+
+	// 1. Bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// 2. Copy our vertices array in a buffer for OpenGL to use
+	// Bind the buffer to a vertex array.
+	// OpenGL allows us to bind to several buffers at once as long as they have a different buffer type.
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Copy the data to the bound buffer
+	// GL_STATIC_DRAW: the data will most likely not change at all or very rarely.
+	// GL_DYNAMIC_DRAW: the data is likely to change a lot.
+	// GL_STREAM_DRAW: the data will change every time it is drawn.
+	// 3. Then set our vertex attributes pointers
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// ARG 0: specifies which vertex attribute we want to configure, layout(location = 0).
+	// ARG 1: specifies the size of the vertex attribute.The vertex attribute is a vec3 so it is composed of 3 values.
+	// ARG 2: specifies the type of the data which is GL_FLOAT(a vec* in GLSL consists of floating point values).
+	// ARG 3: specifies if we want the data to be normalized. If we set this to GL_TRUE all the data that has a value not between 0 (or -1 for signed data) and 1 will be mapped to those values.
+	// ARG 4: known as the stride and tells us the space between consecutive vertex attribute sets.Since the next set of position data is located exactly 3 times the size of a GLfloat away we specify that value as the stride.
+	// Note that since we know that the array is tightly packed(there is no space between the next vertex attribute value) we could've also specified the stride as 0 to let OpenGL determine the stride (this only works when values are tightly packed). 
+	// Whenever we have more vertex attributes we have to carefully define the spacing between each vertex attribute but we'll get to see more examples of that later on.
+	// ARG 5: The last parameter is of type GLvoid* and thus requires that weird cast.This is the offset of where the position data begins in the buffer.
+	// Since the position data is at the start of the data array this value is just 0. We will explore this parameter in more detail later on
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+
+	glEnableVertexAttribArray(0);
+	//4. Unbind the VAO
+	glBindVertexArray(0);
+
+	glBindVertexArray(VAO);
+
+
 
 	// RENDER LOOP
 	while (!glfwWindowShouldClose(window))
@@ -62,10 +185,17 @@ int main()
 		// Clear the colorbuffer
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
-
+	// Properly de-allocate all resources once they've outlived their purpose
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 	glfwTerminate();
 	return 0;
 }
